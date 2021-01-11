4ef85453afb9f7bb23844a596203642911d5ea68
unknown <jp.pfc@hotmail.com>
2021-01-11 02:06:33 -0300

    Add Navbar

---
+++ front-web/src/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+import {ReactComponent as Logo} from '../logo.svg';
+
+function Navbar(){
+    return(
+        <nav className="main-navbar">
+            <Logo /></nav>
+    );
+}
+
+export default Navbar; 
